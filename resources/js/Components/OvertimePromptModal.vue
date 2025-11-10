99797ab846224f98d7c72e6f3459da1515320969
suk <mradovanovic@msn.com>
2025-11-10 13:15:54 +0000

    Uklonjena kartica sa overtime modala

--- resources/js/Components/OvertimePromptModal.vue
+++ resources/js/Components/OvertimePromptModal.vue
@@ -44,16 +44,6 @@
                         >
                             ✓ Да, и даље сам на послу
                         </button>
-
-                        <!-- Warning -->
-                        <div class="mt-4 p-3 bg-gray-50 rounded-lg border border-gray-200">
-                            <p class="text-xs text-gray-600 text-center">
-                                <svg class="inline w-4 h-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                                </svg>
-                                Ова провера се обавља у складу са политиком евиденције радног времена
-                            </p>
-                        </div>
                     </div>
                 </div>
             </div>
